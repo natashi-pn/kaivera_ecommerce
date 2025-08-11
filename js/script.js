3a1624dc64adaf9cadf6a05ef75aa982ba7e3388
natashi <davleo104337@gmail.com>
2025-08-11 19:06:58 +0630

    Loading Font

--- js/script.js
+++ js/script.js
@@ -49,201 +49,202 @@ function setUpHome() {
     lenis.raf(time);
     requestAnimationFrame(raf);
   }
+  document.addEventListener("DOMContentLoaded", () => {
+    document.fonts.ready.then(() => {
+      if (showLoader) {
 
-  document.fonts.ready.then(() => {
-    if (showLoader) {
+        // Disabling scrolling in all platform
+        document.body.style.overflow = "hidden";
+        document.documentElement.style.overflow = "hidden";
+        document.body.addEventListener("touchmove", preventScroll, {
+          passive: false,
+        });
+        document.addEventListener("wheel", preventScroll, { passive: false });
+        lenis.stop();
 
-      // Disabling scrolling in all platform
-      document.body.style.overflow = "hidden";
-      document.documentElement.style.overflow = "hidden";
-      document.body.addEventListener("touchmove", preventScroll, {
-        passive: false,
-      });
-      document.addEventListener("wheel", preventScroll, { passive: false });
-      lenis.stop();
-
-      function preventScroll(e) {
-        e.preventDefault();
-      }
-      sessionStorage.setItem("kaiveraVisited", "true");
-
-
-      // Loader animation
-
-      let counterElement = document.querySelector(".loader .counter p");
-      let currentValue = 0;
-
-      function updateCounter() {
-        if (currentValue < 100) {
-          let increment = Math.floor(Math.random() * 10) + 1;
-          currentValue = Math.min(currentValue + increment, 100);
-          counterElement.textContent = currentValue;
-
-          let delay = Math.floor(Math.random() * 150) + 25;
-          setTimeout(updateCounter, delay);
+        function preventScroll(e) {
+          e.preventDefault();
         }
-      }
+        sessionStorage.setItem("kaiveraVisited", "true");
 
-      const loader_bg = document.querySelectorAll(".loader .loader_bg");
-      const loading_text = document.querySelectorAll(".loader .text_content h1");
-      const arr_text = [];
 
-      loading_text.forEach((el) => {
-        const split = new SplitText(el, {
-          type: "chars"
+        // Loader animation
+
+        let counterElement = document.querySelector(".loader .counter p");
+        let currentValue = 0;
+
+        function updateCounter() {
+          if (currentValue < 100) {
+            let increment = Math.floor(Math.random() * 10) + 1;
+            currentValue = Math.min(currentValue + increment, 100);
+            counterElement.textContent = currentValue;
+
+            let delay = Math.floor(Math.random() * 150) + 25;
+            setTimeout(updateCounter, delay);
+          }
+        }
+
+        const loader_bg = document.querySelectorAll(".loader .loader_bg");
+        const loading_text = document.querySelectorAll(".loader .text_content h1");
+        const arr_text = [];
+
+        loading_text.forEach((el) => {
+          const split = new SplitText(el, {
+            type: "chars"
+          });
+          arr_text.push(split);
         });
-        arr_text.push(split);
-      });
 
-      arr_text.forEach((split) => {
-        gsap.from(split.chars, {
-          y: -100,
+        arr_text.forEach((split) => {
+          gsap.from(split.chars, {
+            y: -100,
+            duration: 1.2,
+            stagger: 0.03,
+            ease: "power4.out",
+            delay: .4
+          });
+
+          gsap.to(split.chars, {
+            y: 50,
+            duration: 1,
+            stagger: 0.02,
+            ease: "power4.in",
+            delay: 2.5
+          })
+        });
+
+        gsap.from(counterElement, {
+          opacity: 0,
+          y: -20,
           duration: 1.2,
-          stagger: 0.03,
           ease: "power4.out",
-          delay: .4
+          delay: .4,
         });
-
-        gsap.to(split.chars, {
-          y: 50,
+        gsap.to(counterElement, {
+          opacity: 0,
+          y: 30,
           duration: 1,
-          stagger: 0.02,
           ease: "power4.in",
           delay: 2.5
         })
-      });
 
-      gsap.from(counterElement, {
-        opacity: 0,
-        y: -20,
-        duration: 1.2,
-        ease: "power4.out",
-        delay: .4,
-      });
-      gsap.to(counterElement, {
-        opacity: 0,
-        y: 30,
-        duration: 1,
-        ease: "power4.in",
-        delay: 2.5
-      })
+        gsap.set(".loader .loader_bg_top, .loader .loader_bg_bottom", {
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
+        });
+        gsap.to(loader_bg, {
+          scale: 0.5,
+          duration: 1.5,
+          ease: "power4.inOut",
+          delay: 2.5,
+        });
+        gsap.to(".loader .loader_bg_top", {
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+          duration: 1.3,
+          ease: "power4.inOut",
+          delay: 3
 
-      gsap.set(".loader .loader_bg_top, .loader .loader_bg_bottom", {
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
-      });
-      gsap.to(loader_bg, {
-        scale: 0.5,
-        duration: 1.5,
-        ease: "power4.inOut",
-        delay: 2.5,
-      });
-      gsap.to(".loader .loader_bg_top", {
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
-        duration: 1.3,
-        ease: "power4.inOut",
-        delay: 3
-
-      });
-
-
-      gsap.to(".loader .loader_bg_bottom", {
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
-        duration: 1.3,
-        ease: "power4.inOut",
-        delay: 3.4,
-
-        onComplete: () => {
-          document.querySelector('.loader').style.display = "none";
-          document.body.style.overflow = "";
-          document.documentElement.style.overflow = "";
-          document.body.removeEventListener("touchmove", preventScroll);
-          document.removeEventListener("wheel", preventScroll);
-          lenis.start();
-          requestAnimationFrame(raf);
-
-        },
-      });
-
-
-      gsap.to(".loader", {
-        delay: 6,
-        onComplete: () => {
-          document.querySelector('.loader').style.display = "none";
-        },
-      });
-
-
-      updateCounter();
-      startMainAnimation(3);
-
-
-
-    } else {
-      document.querySelector(".hero-img").style.display = "none"
-      document.querySelector(".loader").style.display = "none";
-      lenis.start();
-      requestAnimationFrame(raf);
-      startMainAnimation(0);
-
-    }
-
-    function startMainAnimation(delay = 0) {
-
-      console.log("start animation called with " + delay)
-      const heroTexts = document.querySelectorAll(".landing-page h1");
-      const heroSplit = [];
-
-
-      heroTexts.forEach((el) => {
-        const split = new SplitText(el, {
-          type: "chars",
-          autoSplit: true,
         });
 
-        heroSplit.push(split);
-      });
-      heroSplit.forEach((split) => {
-        gsap.from(split.chars, {
-          y: 200,
-          duration: 2,
-          ease: "power4.out",
-          stagger: 0.1,
-          delay: delay + 0.5,
+
+        gsap.to(".loader .loader_bg_bottom", {
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+          duration: 1.3,
+          ease: "power4.inOut",
+          delay: 3.4,
+
           onComplete: () => {
-            document.body.style.overflowX = "hidden";
+            document.querySelector('.loader').style.display = "none";
+            document.body.style.overflow = "";
+            document.documentElement.style.overflow = "";
+            document.body.removeEventListener("touchmove", preventScroll);
+            document.removeEventListener("wheel", preventScroll);
+            lenis.start();
+            requestAnimationFrame(raf);
+
           },
         });
-      });
-      gsap.set(".hero-img", {
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-        scale: 1.3,
-      });
-      gsap.to(".hero-img", {
-        scale: 1,
-        duration: 2,
-        ease: "power4.inOut",
-        delay: delay
-      })
-      gsap.set(".hero-video .video", {
-        clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
-      })
-      gsap.to(".hero-video .video", {
 
-        duration: 1.5,
-        ease: "power4.inOut",
-        delay: delay + 2,
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-      })
-      gsap.to(".hero-img", {
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
-        duration: 2,
-        ease: "power4.inOut",
-        delay: delay + .2,
-        onComplete: () => {
-          document.querySelector(".hero-img").style.display = "none";
-        }
-      })
-    }
+
+        gsap.to(".loader", {
+          delay: 6,
+          onComplete: () => {
+            document.querySelector('.loader').style.display = "none";
+          },
+        });
+
+
+        updateCounter();
+        startMainAnimation(3);
+
+
+
+      } else {
+        document.querySelector(".hero-img").style.display = "none"
+        document.querySelector(".loader").style.display = "none";
+        lenis.start();
+        requestAnimationFrame(raf);
+        startMainAnimation(0);
+
+      }
+
+      function startMainAnimation(delay = 0) {
+
+
+        const heroTexts = document.querySelectorAll(".landing-page h1");
+        const heroSplit = [];
+
+
+        heroTexts.forEach((el) => {
+          const split = new SplitText(el, {
+            type: "chars",
+            autoSplit: true,
+          });
+
+          heroSplit.push(split);
+        });
+        heroSplit.forEach((split) => {
+          gsap.from(split.chars, {
+            y: 200,
+            duration: 2,
+            ease: "power4.out",
+            stagger: 0.1,
+            delay: delay + 0.5,
+            onComplete: () => {
+              document.body.style.overflowX = "hidden";
+            },
+          });
+        });
+        gsap.set(".hero-img", {
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          scale: 1.3,
+        });
+        gsap.to(".hero-img", {
+          scale: 1,
+          duration: 2,
+          ease: "power4.inOut",
+          delay: delay
+        })
+        gsap.set(".hero-video .video", {
+          clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
+        })
+        gsap.to(".hero-video .video", {
+
+          duration: 1.5,
+          ease: "power4.inOut",
+          delay: delay + 2,
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        })
+        gsap.to(".hero-img", {
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+          duration: 2,
+          ease: "power4.inOut",
+          delay: delay + .2,
+          onComplete: () => {
+            document.querySelector(".hero-img").style.display = "none";
+          }
+        })
+      }
+    })
   });
 
   // Scroll Trigger
